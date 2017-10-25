8d2f259f0d4a922d4f6d14a9bc250659773565f8
kramdolf407 <pfpeterflodmark@gmail.com>
2017-10-25 10:06:56 +0200

    Initial commit

---
+++ server/Socket_handler.py
@@ -0,0 +1,34 @@
+import socket
+import _thread
+
+class Socket_handler:
+    def __init__(self):
+        self.serverSocket= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.serverSocket.bind(('',9999))
+        self.serverSocket.listen()
+        self.list_of_sockets = []
+
+    def init_the_view_obj(self,chattViewer_):
+        self.chattViewer = chattViewer_
+
+    def acceptConnection(self):
+        clientSocket, addr = self.serverSocket.accept()
+        self.list_of_sockets.append(clientSocket)
+
+        self.startReceiver(clientSocket)
+
+        #starta recev
+
+    def sendMsg(self,text):
+        for soc in self.list_of_sockets:
+            soc.send(str.encode(text))
+
+    def startReceiver(self, csock):
+        _thread.start_new_thread(self.func_to_receiver,(csock,))
+
+    def func_to_receiver(self,csock):
+        while True:
+            for csock in self.list_of_sockets:
+                msg = csock.recv(1024).decode()
+                self.chattViewer.showMessage(msg)
+                csock.send(str.encode(msg))
---
+++ server/main.py
@@ -0,0 +1,20 @@
+from server.ChattViewer import ChattViewer
+from server.Socket_handler import Socket_handler
+import _thread
+
+def funcToThread():
+    while True:
+        sock_handler.acceptConnection()
+
+
+sock_handler = Socket_handler()
+
+chattV = ChattViewer(sock_handler)
+chattV.buildGui()
+
+sock_handler.init_the_view_obj(chattV)
+
+_thread.start_new_thread(funcToThread, ())
+
+chattV.start()
+
